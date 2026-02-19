3f74a5690f1e81cab5d51ad3246509e40948d3ba
Shaurya Thakur <176936155+Shaurya0987@users.noreply.github.com>
2026-02-19 17:16:53 +0530

    Weather Api Added

--- lib/Provider/WeatherProvider.dart
+++ lib/Provider/WeatherProvider.dart
@@ -1,7 +1,8 @@
+import 'package:api/Services/ApiService.dart';
 import 'package:flutter/material.dart';
 
 class WeatherProvider extends ChangeNotifier{
 
-  
+  ApiService service = ApiService();
 
 }
