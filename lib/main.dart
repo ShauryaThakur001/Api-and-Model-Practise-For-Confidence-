fd2cb7d48755903fe18c9e2058a4c117ec989afa
Shaurya Thakur <176936155+Shaurya0987@users.noreply.github.com>
2026-02-20 20:09:16 +0530

    Colors  Added

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Weather Api Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.blue),
       ),
       home: CurrencyScreen(),
     );
